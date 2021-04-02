14d36d2a5d70688454cb4aeb3200e4af160616aa
spothini <Syambabu.Pothini@siriusxm.com>
2021-04-01 19:59:54 -0500

    update jenkins installation steps

--- provision.sh
+++ provision.sh
@@ -6,9 +6,8 @@ VAGRANT_HOST_DIR=/mnt/host_machine
 # Jenkins & Java
 ########################
 echo "Installing Jenkins and Java"
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FCEF32E745F2C3D5 > /dev/null 2>&1
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y install default-jdk jenkins > /dev/null 2>&1
 echo "Installing Jenkins default user and config"
